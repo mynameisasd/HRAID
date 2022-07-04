09a3004a901028708f8771d29aec3c0430e5e999
Jerick Buitizon <magolda1974@gmail.com>
2022-07-04 15:39:37 +0800

    save css style

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { Container, Row, Col, Nav, Navbar, NavDropdown } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
+import GlobalFooter from "./GlobalFooter";
 
 
 import GlobalNavigation from './GlobalNavigation'
@@ -9,7 +10,27 @@ const Dashboard = () => {
     return (
         <Container>
             <GlobalNavigation/>
-            <h1>Dashboard</h1>
+            <br/>
+            <div className="dashboard-content">
+                <h2 style={{textAlign:"left"}}>DASHBOARD</h2>
+
+                <Row>
+                    <Col md="8">
+                        <div style={{background:"white"}}>
+                            <h4>Content</h4>
+                        </div>
+                       
+                    </Col>
+                    <Col md="4">
+                        <div style={{background:"white"}}>
+                            <h4>Side Content</h4>
+                        </div>
+                    </Col>
+                </Row>
+
+            </div>
+            <br/>
+            <GlobalFooter/>
         </Container>
     )
 }
