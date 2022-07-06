953abc63fc12a2c9c40d11de90e08df74334f269
Jerick Buitizon <magolda1974@gmail.com>
2022-07-06 14:52:05 +0800

    update array on desired Position

--- src/components/AddEmployee.js
+++ src/components/AddEmployee.js
@@ -1,10 +1,42 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Container, Row, Col, FloatingLabel, Form, ListGroup, Button } from 'react-bootstrap'
+
+import { useForm } from 'react-hook-form'
 
 import GlobalNavigation from './GlobalNavigation'
 import GlobalFooter from './GlobalFooter'
 
 const AddEmployee = () => {
+
+    const {register, handleSubmit, errors } = useForm();
+
+    // <------------------------ Desired Position Array
+    const [ desiredPosition, setDesiredPosition ] = useState([])
+    const [ newDesiredPosition, setNewDesiredPosition ] = useState('')
+
+    function handleNewDesiredPositionChange(e){
+
+        setNewDesiredPosition(e.target.value);
+        
+    }
+
+    function addDesiredPositionToArray(){
+        
+            let arr = desiredPosition;
+            arr.push(newDesiredPosition);
+            setNewDesiredPosition('');
+    }
+
+ 
+    //----------------->
+
+
+
+    //submit the form using react hook form
+    const onSubmit = (data  ) => {
+        
+        console.log(data)
+    }
 
     return (
         <Container>
@@ -14,12 +46,14 @@ const AddEmployee = () => {
             <div className='form-container'>
                 <h2 style={{textAlign:"left"}}>ADD EMPLOYEE</h2>
                 <br/>
-                <Form>
+
+                <form onSubmit={handleSubmit(onSubmit)}>
 
                     <Row className='g-2'>
                         <Col md="3">
                             <FloatingLabel controlId="daterecieved" label="Date Recieved">
-                                <Form.Control size='sm' type="date" placeholder="Date" />
+                                <Form.Control size='sm' type="date" placeholder="Date" name="dateReceived" {...register('dateReceived')} required />
+                               
                             </FloatingLabel>
                         </Col>
                         <Col md=""></Col>
@@ -28,49 +62,91 @@ const AddEmployee = () => {
                     <Row className='g-2'>
                         <Col md="3" sm="12" xs="12">
                             <FloatingLabel controlId="firstname" label="First Name">
-                                <Form.Control size='sm' type="text" placeholder="First Name" required />
+                                <Form.Control size='sm' type="text" placeholder="First Name" name="firstname" {...register('firstname')} required />
                             </FloatingLabel>
                         </Col>
                         <Col md="3">
                             <FloatingLabel controlId="middlename" label="Middle Name">
-                                <Form.Control size='sm' type="text" placeholder="Middle Name" />
+                                <Form.Control size='sm' type="text" placeholder="Middle Name" name="middlename" {...register('middlename')} />
                             </FloatingLabel>
                         </Col>
                         <Col md="3">
                             <FloatingLabel controlId="lastname" label="Last Name">
-                                <Form.Control size='sm' type="text" placeholder="Last Name" />
+                                <Form.Control size='sm' type="text" placeholder="Last Name" name="lastname" {...register('middlename')} required />
                             </FloatingLabel>
                         </Col>
                         <Col md="3">
                             <FloatingLabel controlId="suffix" label="Suffix">
-                                <Form.Control size='sm' type="text" placeholder="Suffix" />
-
+                                <Form.Control size='sm' type="text" placeholder="Suffix" name="suffix" {...register('middlename')} />
                             </FloatingLabel>
                         </Col>
                     </Row>
                     <br/>
                     <Row className='g-2'>
-                        <Col>
+                        <Col md="5">
                             <FloatingLabel controlId="desiredposition" label="Desired Position">
-                                <Form.Control size='sm' type="text" placeholder="Desired Position" />
+                                <Form.Control size='sm' type="text" placeholder="Desired Position" value={newDesiredPosition} onChange={handleNewDesiredPositionChange} />
                             </FloatingLabel>
                         </Col>
+                        <Col md="1">
+                            <button size="sm" style={{color:"white",}} type="button" class="btn btn-info" onClick={addDesiredPositionToArray} >ADD</button>
+                        </Col>
+                        <Col md="6">
+                            <div style={{padding:'15px', background:'white'}}>
+                                <h6>Desired Position</h6>
+                                <ListGroup>
+                                    {
+                                        desiredPosition.map((position, index) =>
+                                        <ListGroup.Item>
+                                            <Row >           
+                                                <Col md="8">
+                                                    <div className='align-content-center'>
+                                                        {position}
+                                                    </div>
+                                                </Col>
+                                                <Col md="4">
+                                                    <div style={{display:"inline-grid",gridGap:"10px"}}>
+
+                                                        <Button 
+                                                            variant="danger" 
+                                                            size="sm" 
+                                                            key={index} 
+                                                            onClick={()=>{
+                                                                let arr = [...desiredPosition]
+                                                                let i = arr.indexOf(index)
+                                                                arr.splice(i, 1);
+                                                                setDesiredPosition(arr)
+                                                                console.log(desiredPosition)
+                                                            }}>
+                                                            REMOVE
+                                                        </Button>
+                                                        
+                                                    </div>
+                                                </Col>
+                                            </Row>
+                                        </ListGroup.Item>
+                                        )
+                                    }
+                                </ListGroup>
+                            </div>
+                        </Col>
+                      
                     </Row>
                     <br/>
                     <Row className='g-2'>
                         <Col md="6">
                             <FloatingLabel controlId="address" label="Address">
-                                <Form.Control size='sm' type="text" placeholder="Address" />
+                                <Form.Control size='sm' type="text" placeholder="Address" name="address" {...register('address')} required />
                             </FloatingLabel>
                         </Col>
                         <Col md="3">
                             <FloatingLabel controlId="emailaddress" label="Email Address">
-                                <Form.Control size='sm' type="text" placeholder="Email Address" />
+                                <Form.Control size='sm' type="text" placeholder="Email Address" name="emailaddress" {...register('emailaddress')} />
                             </FloatingLabel>
                         </Col>
                         <Col md="3">
                             <FloatingLabel controlId="contactnumber" label="Contact Number">
-                                <Form.Control size='sm' type="text" placeholder="Contact Number" />
+                                <Form.Control size='sm' type="text" placeholder="Contact Number" name="contactnumber" {...register('contactnumber')}/>
                             </FloatingLabel>
                         </Col>
                     </Row>
@@ -78,12 +154,12 @@ const AddEmployee = () => {
                     <Row className='g-2'>
                         <Col md="3">
                             <FloatingLabel controlId="birthday" label="Birthday">
-                                <Form.Control size='sm' type="date" placeholder="Birthday" />
+                                <Form.Control size='sm' type="date" placeholder="Birthday" name="birthday"  {...register('birthday')} required />
                             </FloatingLabel>
                         </Col>
                         <Col md="3">
                             <FloatingLabel controlId="floatingSelectGrid" label="Gender">
-                            <Form.Select aria-label="Floating label select example">
+                            <Form.Select aria-label="Floating label select example" name="genter" {...register("gender")} >
                                 <option>Open this select menu</option>
                                 <option value="Male">Male</option>
                                 <option value="Female">Female</option>
@@ -92,7 +168,7 @@ const AddEmployee = () => {
                         </Col>
                         <Col md="3">
                             <FloatingLabel controlId="religion" label="Religion">
-                                <Form.Control size='sm' type="text" placeholder="Religion" />
+                                <Form.Control size='sm' type="text" placeholder="Religion" name="religion" {...register('religion')}   />
                             </FloatingLabel>
                         </Col>
                     </Row>
@@ -100,7 +176,7 @@ const AddEmployee = () => {
                     <Row className='g-2'>
                         <Col>
                             <FloatingLabel controlId="highesteducationalattainment" label="Highest Educational Attainment">
-                                <Form.Control size='sm' type="text" placeholder="Highest Educational Attainment" />
+                                <Form.Control size='sm' type="text" placeholder="Highest Educational Attainment" name="highesteducationalattainment" {...register('highesteducationalattainment')} />
                             </FloatingLabel>
                         </Col>
                     </Row>
@@ -210,17 +286,17 @@ const AddEmployee = () => {
                     <Row className='g-2'>
                         <Col>
                             <FloatingLabel controlId="eperience" label="Experience (No. of Years)">
-                                <Form.Control size='sm' type="text" placeholder="Experience (No. of Years)"/>
+                                <Form.Control size='sm' type="text" placeholder="Experience (No. of Years)" name="experience" {...register('experience')}  />
                             </FloatingLabel>
                         </Col>
                         <Col>
                             <FloatingLabel controlId="dataonpersonwithdisability" label="Data on Persons With Disability">
-                                <Form.Control size='sm' type="text" placeholder="Data on Persons With Disability" />
+                                <Form.Control size='sm' type="text" placeholder="Data on Persons With Disability" name="dataonpersonwithdisability" {...register('dataonpersonwithdisability')} />
                             </FloatingLabel>
                         </Col>
                     </Row>
                     <hr/>
-                    <Row className='g-2'>
+                    {/* <Row className='g-2'>
                         <Col md="2">
                             <Form.Check aria-label="option 1" label="PASS INITIAL ASSESSMENT?" />
                         </Col>
@@ -239,15 +315,15 @@ const AddEmployee = () => {
                         <Col md="2">
                             <Form.Check aria-label="option 1" label="ISSUED APPT" />
                         </Col>
-                    </Row>
-                    <hr/>
+                    </Row> */}
+                  
                     <Row className='g-2'>
                         <Col style={{textAlign:'right'}}>
-                            <button size="sm" style={{color:"white",}} type="button" class="btn btn-success">SUBMIT</button>
+                            <button size="sm"  style={{color:"white",}} type="submit" class="btn btn-success">SUBMIT</button>
                         </Col>
                     </Row>
 
-                </Form>
+                </form>
             </div>
             <br />
             <GlobalFooter/>
--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,11 +1,20 @@
-import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
+import React, { useState } from "react";
+import { Container, Row, Col, Carousel } from "react-bootstrap";
 import GlobalFooter from "./GlobalFooter";
 
 
 import GlobalNavigation from './GlobalNavigation'
 
+
+import MyImage from '../img/User-avatar.png';
+
+
+
 const Dashboard = () => {
+
+    const [ eligibility , setEligibility ] = useState([]);
+
+
 
     return (
         <Container>
@@ -17,12 +26,50 @@ const Dashboard = () => {
                 <Row>
                     <Col md="8">
                         <div style={{background:"white"}}>
-                            <h4>Content</h4>
+                            <Carousel interval={4000} style={{background:'lightblue'}}>
+                                <Carousel.Item>
+                                    <Row>
+                                        <Col md="4">
+                                            <img
+                                                className="d-block w-100"
+                                                src={MyImage}
+                                                alt="First slide"
+                                            />
+                                        </Col>
+                                        <Col md="8">
+                                            <div style={{textAlign:"left",padding:'15px'}}>
+                                                <h2>Juan Dela Cruz</h2>
+                                                <p>Address: Puerto, Princesa City, Palawan</p>
+                                                <p>Contact No.: 09298468754</p>
+                                                <p>Email Add.: juan.delacruz@gmail.com</p>
+                                            </div>
+                                        </Col>
+                                    </Row>
+                                </Carousel.Item>
+                                <Carousel.Item>
+                                    <Row>
+                                        <Col md="4">
+                                            <img
+                                                className="d-block w-100"
+                                                src={MyImage}
+                                                alt="First slide"
+                                            />
+                                        </Col>
+                                        <Col md="8">
+                                            <div style={{textAlign:"left",padding:'15px'}}>
+                                                <h2>Juan Dela Cruz 2</h2>
+                                                <p>Address: Puerto, Princesa City, Palawan</p>
+                                                <p>Contact No.: 09298468754</p>
+                                                <p>Email Add.: juan.delacruz@gmail.com</p>
+                                            </div>
+                                        </Col>
+                                    </Row>
+                                </Carousel.Item>
+                            </Carousel>
                         </div>
-                       
                     </Col>
                     <Col md="4">
-                        <div style={{background:"white"}}>
+                        <div style={{background:"white", padding:'15px'}}>
                             <h4>Side Content</h4>
                         </div>
                     </Col>
